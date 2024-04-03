84331c657392ca9f03782359d7a2528b115f4172
Omar Selim <ac145347@avcol.school.nz>
2024-04-03 15:08:38 +1300

    Created models and relationships

---
+++ ReginusLTD/Models/Booking.cs
@@ -0,0 +1,15 @@
+﻿using ReginusLTD.Areas.Identity.Data;
+
+namespace ReginusLTD.Models
+{
+    public class Booking
+    {
+        public int bookingId { get; set; }
+        public int UserId { get; set; }
+        public int CounsellorId { get; set; }
+        public DateTime Date { get; set; }
+
+        public Counsellor Counsellor { get; set; }
+        public ReginusLTDUser ReginusLTDUser { get; set; }
+    }
+}
---
+++ ReginusLTD/Models/Counsellor.cs
@@ -0,0 +1,14 @@
+﻿namespace ReginusLTD.Models
+{
+    public class Counsellor
+    {
+        public int CounsellorId { get; set; }
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public ICollection<Counsellor> Counsellors { get; set; }
+
+    }
+}
